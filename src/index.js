1b06ec0c48b3082f5c605b38bd1c4ada7e773df9
kerberry <trial404@mail.ru>
2022-01-09 19:30:59 +0300

    feat:20 passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n);
+    let result = '';
+    for(let i = str.length - 1; i > 0; i --){
+        if(str[str.length - 1] == 0){
+            i = str.length - 2;
+        }
+        result += str[i];
+    }
+    if (str[0] != '-'){
+        result += str[0];
+    }
+    return Number(result);
 }
